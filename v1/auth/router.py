2b585e7dcdd01e09b51e4a003d63ed524008166a
Semyon Aparin <sem0156@mail.ru>
2022-06-08 14:40:48 +0300

    add logout endpoint

--- v1/auth/router.py
+++ v1/auth/router.py
@@ -32,3 +32,15 @@ async def login(
     db: Session = Depends(get_db)
 ):
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not implemented")
+
+
+@router.post(
+    path='/logout',
+    summary='Выход из аккаунта',
+    response_model=ResponseSuccess
+)
+async def logout(
+    # TODO: userSession here
+    db: Session = Depends(get_db)
+):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not implemented")
