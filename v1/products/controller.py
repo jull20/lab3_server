4fef7f9161d8b74a179155e353e9b7a80ed1e04b
Semyon Aparin <sem0156@mail.ru>
2022-06-09 14:37:26 +0300

    get all product and by id implemented

--- v1/products/controller.py
+++ v1/products/controller.py
@@ -1,10 +1,39 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
+
+from models.product import Product
+from models.rate import Rate
+from models.order import Order
+from v1.products.schema import ResponseProduct
 
 
 async def get_all_products_service(db: Session):
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not implemented")
+    products: list[Product] = db.query(Product.id, Product.name, Product.photo, Product.price, Product.start_amount, func.sum(Order.amount), func.avg(Rate.value)).join(Order, Order.product_id == Product.id, isouter=True).join(Rate, Rate.product_id == Product.id, isouter=True).group_by(Product.id).all()
+    
+    return list(map(
+        lambda x: ResponseProduct(
+            id=x.id,
+            name=x.name, 
+            photo=x.photo, 
+            price=x.price,
+            remaind=x.start_amount - (x[5] if x[5] else 0),
+            rate=x[6]
+        ),
+        products
+    ))
 
 
 async def get_product_by_id_service(db: Session, productId: int):
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not implemented")
+    try:
+        product: Product = db.query(Product.id, Product.name, Product.photo, Product.price, Product.start_amount, func.sum(Order.amount), func.avg(Rate.value)).filter(Product.id == productId).join(Order, Order.product_id == Product.id, isouter=True).join(Rate, Rate.product_id == Product.id, isouter=True).group_by(Product.id).one()
+        return ResponseProduct(
+            id=product.id,
+            name=product.name,
+            photo=product.photo,
+            price=product.price,
+            remaind=product.start_amount - (product[5] if product[5] else 0),
+            rate=product[6]
+        )
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product with same id not found")
--- v1/products/schema.py
+++ v1/products/schema.py
@@ -2,9 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class ResponseProduct(BaseModel):
+    id: int = Field(default=1, description='Идентификатор продукта')
     name: str = Field(default='AirPods Max')
     photo: str = Field(default='https://vk.com/good_photo.jpg')
-    rate: float = Field(default=4.75)
+    rate: float | None = Field(default=4.75, description='Средний рейтинг товара, может быть значение null - когда его еще не оценили')
     remaind: int = Field(default=26)
     price: float = Field(default=76.99)
 
