9f244a047975526235276b1bbe411f77e1eb5852
Semyon Aparin <sem0156@mail.ru>
2022-06-15 22:14:24 +0300

    cors

--- core/settings/tune.py
+++ core/settings/tune.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from v1 import authRouter, productsRouter, reportRouter, userRouter
 
 def set_routers(app: FastAPI):
@@ -13,4 +14,19 @@ def use_session(app: FastAPI):
     app.add_middleware(
         SessionMiddleware,
         secret_key="Supersecret_key",
+    )
+
+
+def use_cors(app: FastAPI):
+    """ Setup CORS setting """
+    origins = [
+        "http://localhost:8080"
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn  
-from core.settings.tune import set_routers, use_session
+from core.settings.tune import set_routers, use_session, use_cors
 from core.settings.environment import PORT, DEBUG
 from core.settings.database import Base, engine
 from models import *
@@ -12,6 +12,7 @@ app = FastAPI()
 
 set_routers(app=app)
 use_session(app=app)
+use_cors(app=app)
 app.mount("/public", StaticFiles(directory="public"), name="site")
 
 if __name__ == "__main__":
